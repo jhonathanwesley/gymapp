e7b569cfab8ea997f1fc6f0324f631b46746cabe
Jhonathan Wesley Faria Dias Alves <jhonwfdalves@gmail.com>
2025-11-14 15:40:44 -0300

    Commit Automático

--- database/instructors.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO webuser.TB_PLANS (plan) VALUES ('Monthly Plan');
-INSERT INTO webuser.TB_PLANS (plan) VALUES ('Quarterly Plan');
-INSERT INTO webuser.TB_PLANS (plan) VALUES ('Semester Plan');
-INSERT INTO webuser.TB_PLANS (plan) VALUES ('Annual Plan');
